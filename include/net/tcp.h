0fcbbf62bda24e131afddd64cc7618e2031cfc7b
~jkoo <hajekmatej21@gmail.com>
2026-05-09 10:21:30 +0200

    fix typo smh

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -54,7 +54,7 @@ int tcp_orphan_count_sum(void);
 void tcp_time_wait(struct sock *sk, int state, int timeo);
 
 #define MAX_TCP_HEADER	L1_CACHE_ALIGN(128 + MAX_HEADER)
-#define MAX_TCP_OPTION_SPACE 40f
+#define MAX_TCP_OPTION_SPACE 40
 #define TCP_MIN_SND_MSS		48
 #define TCP_MIN_GSO_SIZE	(TCP_MIN_SND_MSS - MAX_TCP_OPTION_SPACE)
 
